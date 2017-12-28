dff15723a3f9441961a2701732e9864ae846788d
Anton_Isakov <Anton_Isakov@epam.com>
2017-12-28 12:42:57 +0400

    Gulp paths file added

--- html-task/gulpfile.js
+++ html-task/gulpfile.js
@@ -3,16 +3,40 @@ const sass  = require('gulp-sass');
 //const babel = require('gulp-babel');
 const gulpCopy = require('gulp-copy');
 
+const paths = {
+  src: {
+    scss: {
+      folder: './src/scss/**/*.scss',
+      main:   './src/scss/main.scss'
+    },
+
+    js:   './src/js/*.js',
+    html: './src/*.html',
+    img:  './src/images/**/*'
+  },
+
+  dist: {
+    folder: './dist/',
+    scss:   './dist/css/',
+    js:     './dist/js/',
+    img:    './dist/images/'
+  }
+};
+
+//const sourceFiles = [ './src/*.html', './src/images/*', './src/js/*.js' ];
+//const destination = './dist/';
+
 gulp.task('sass', function() {
-  return gulp.src('./src/scss/main.scss')
+  return gulp.src(paths.src.scss.main)
     .pipe(sass(/*{
       outputStyle: 'compressed'
     }*/).on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest(patsh.dist.scss));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/scss/**/*.scss', ['sass']);
+  gulp.watch(paths.src.scss.folder, ['sass']);
+  gulp.watch([paths.src.html, paths.src.img, paths.src.js], ['copy']);
 });
 
 // gulp.task('babel', () =>
@@ -26,13 +50,10 @@ gulp.task('watch', function() {
 gulp.task('default', ['sass', 'watch'/*, 'babel'*/]);
 
 gulp.task('copy', function() {
-  const sourceFiles = [ './src/*.html', './src/images/*', './src/js/*.js' ];
-  const destination = './dist/';
-
-  gulp.src('./src/*.html')
-      .pipe(gulp.dest('./dist/'));
-  gulp.src('./src/images/**/*')
-      .pipe(gulp.dest('./dist/images/'));
-  gulp.src('./src/js/*.js')
-      .pipe(gulp.dest('./dist/js/'));
+  gulp.src(paths.src.html)
+      .pipe(gulp.dest(paths.dist.folder));
+  gulp.src(paths.src.img)
+      .pipe(gulp.dest(paths.dist.img));
+  gulp.src(paths.src.js)
+      .pipe(gulp.dest(paths.dist.js));
 });
