1e1c87cf0484b1b5571a88705098efb8e828bbe9
Anton_Isakov <Anton_Isakov@epam.com>
2018-01-09 15:57:46 +0400

    task

--- html-task/gulpfile.js
+++ html-task/gulpfile.js
@@ -2,6 +2,17 @@ const gulp  = require('gulp');
 const sass  = require('gulp-sass');
 //const babel = require('gulp-babel');
 const gulpCopy = require('gulp-copy');
+const webserver = require('gulp-webserver');
+//const browserSync = require('browser-sync').create();
+
+const imagemin = require('gulp-imagemin');
+
+const browserify = require('browserify');
+const watchify = require('watchify');
+const source = require('vinyl-source-stream');
+//const sourceJsFile = './js/main.js';
+//const destFolder = paths.dist.js;
+//const destFile = 'findem.js';
 
 const paths = {
   src: {
@@ -10,7 +21,7 @@ const paths = {
       main:   './src/scss/main.scss'
     },
 
-    js:   './src/js/**/*.js',
+    js:   './src/js/main.js',
     html: './src/**/*.html',
     img:  './src/images/**'
   },
@@ -18,7 +29,10 @@ const paths = {
   dist: {
     folder: './dist/',
     scss:   './dist/css/',
-    js:     './dist/js/',
+    js: {
+      folder: './dist/js/',
+      bundle: 'bundle.js'
+    },
     img:    './dist/images/'
   }
 };
@@ -39,7 +53,7 @@ gulp.task('watch', function() {
 
   gulp.watch(paths.src.html, ['copyHtml']);
   gulp.watch(paths.src.img, ['copyImg']);
-  gulp.watch(paths.src.js, ['copyJs']);
+  //gulp.watch(paths.src.js, ['copyJs']);
 });
 
 // gulp.task('babel', () =>
@@ -59,10 +73,35 @@ gulp.task('copyHtml', function() {
 
 gulp.task('copyImg', function() {
   gulp.src(paths.src.img)
+      .pipe(imagemin())
       .pipe(gulp.dest(paths.dist.img));
 });
 
-gulp.task('copyJs', function() {
-  gulp.src(paths.src.js)
-      .pipe(gulp.dest(paths.dist.js));
+// gulp.task('copyJs', function() {
+//   gulp.src(paths.src.js)
+//       .pipe(gulp.dest(paths.dist.js));
+// });
+ 
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: "dist/index.html"
+    }));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+});
+ 
+gulp.task('browserify', function() {
+  return browserify(paths.src.js)
+  .bundle()
+  .pipe(source(paths.dist.js.bundle))
+  .pipe(gulp.dest(paths.dist.js.folder));
 });
---
+++ html-task/src/js/main.js
@@ -0,0 +1,9 @@
+var _ = require('underscore');
+var $ = require('jquery');
+
+window.$ = window.jQuery = $;
+window._ = window.underscore = _;
+
+//var $ = require('jquery');
+//global.jQuery = require("jquery");
+//import $ from "jquery";
