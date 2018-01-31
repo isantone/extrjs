8007474e35e0760d9c68795f180cf89e71142006
Anton_Isakov <anton_isakov@epam.com>
2018-01-31 15:20:37 +0400

    Css and design corrections

--- html-task/dist/js/menu.js
+++ html-task/dist/js/menu.js
@@ -1,7 +1,18 @@
 function toggleNav() {
-  navButton.classList.toggle('header__animation');
+  //navButton.classList.toggle('header__animation');
     //navigationMenu.style.maxHeight = "500px"; //280px
-  navigationMenu.classList.toggle('navigation_visible');
+  //navigationMenu.classList.toggle('navigation_visible');
+  let navBtn = document.getElementById('navBtn');
+  let navBtnTop = document.getElementsByClassName('nav-btn__top')[0];
+  let navBtnMid = document.getElementsByClassName('nav-btn__mid')[0];
+  let navBtnBot = document.getElementsByClassName('nav-btn__bot')[0];
+  
+  navBtn.addEventListener('click', function(e) {
+    this.classList.toggle('nav-btn_opened');
+    navBtnTop.classList.toggle('nav-btn__top_opened');
+    navBtnMid.classList.toggle('nav-btn__mid_opened');
+    navBtnBot.classList.toggle('nav-btn__bot_opened');
+  });
 }
 
 //var searchClicked = false;
