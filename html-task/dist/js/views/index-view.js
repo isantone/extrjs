e8fc661a6fd049a5f0eb926e01f06012eb6629c6
Anton Isakov <anton_isakov@epam.com>
2018-02-02 00:03:07 +0400

    Header and categories redesign

--- html-task/dist/js/views/index-view.js
+++ html-task/dist/js/views/index-view.js
@@ -3,11 +3,19 @@ export default class IndexView {
     const categoryTemplate = `
       <main id="pageContent" class="index-page">
         {{#each this}}
-          <div class="grid index-page__grid">
-            <a href="#categories/{{this.name}}/products">
-              <img class="grid__image index-page__image" src="{{this.image}}">
-              <h2 class="grid__caption index-page__caption tiny-top-margin">{{this.title}}</h2>
-            </a>
+          <div class="catalog__container">
+            <div class="catalog__header-container">
+              <a href="#categories/{{this.name}}/products">
+                <h2>{{this.title}}</h2>
+                <!--<img class="" src="{{this.image}}">-->
+              </a>
+            </div>
+            {{#each catalog}}
+              <div class="catalog__item-wrapper">
+                <img class="catalog__image" src="{{this.image}}">
+                <a class="catalog__caption" href="#categories/{{this.name}}/products">{{this.title}}</a>
+              </div>
+            {{/each}}
           </div>
         {{/each}}
       </main>
