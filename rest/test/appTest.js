c5d4d815b90b48746da126fec8e7a0b5bb1f765e
Anton_Isakov <anton_isakov@epam.com>
2018-02-01 14:50:59 +0400

    Test environment sets

--- rest/test/appTest.js
+++ rest/test/appTest.js
@@ -1,5 +1,8 @@
+process.env.MODE = 'test';
+
 const assert = require('chai').assert;
 const app = require('../app');
+const stop = require('../app').stop;
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -10,18 +13,18 @@ const users = require('../data/users');
 const catalog = require('../data/catalog');
 const products = require('../data/products');
 
-const host = 'http://localhost:3000/api';
+//const host = 'http://localhost:3000/api';
 
 chai.use(chaiHttp);
 
 describe('App', function() {
   after(() => {
-    //process.exit();
+    stop();
   });
 
   it('it should GET users', (done) => {
-    chai.request(host)
-      .get('/users')
+    chai.request(app)
+      .get('/api/users')
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
@@ -33,8 +36,8 @@ describe('App', function() {
   });
 
   it('it should GET catalog', (done) => {
-    chai.request(host)
-      .get('/categories')
+    chai.request(app)
+      .get('/api/categories')
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
@@ -46,8 +49,8 @@ describe('App', function() {
   });
 
   it('it should GET catalog', (done) => {
-    chai.request(host)
-      .get('/categories')
+    chai.request(app)
+      .get('/api/categories')
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
@@ -59,8 +62,8 @@ describe('App', function() {
   });
 
   it('it should GET first product', (done) => {
-    chai.request(host)
-      .get('/products/1')
+    chai.request(app)
+      .get('/api/products/1')
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
