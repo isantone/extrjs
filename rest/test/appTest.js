4ea6e20d9de2d3039e4366feece6d71f6bb42696
Anton_Isakov <anton_isakov@epam.com>
2018-02-02 16:42:26 +0400

    Corrections of tests

--- rest/test/appTest.js
+++ rest/test/appTest.js
@@ -8,6 +8,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 //const server = require('../server');
 const should = chai.should();
+const expect = chai.expect();
 
 const users = require('../data/users');
 const catalog = require('../data/catalog');
@@ -17,46 +18,34 @@ const products = require('../data/products');
 
 chai.use(chaiHttp);
 
-describe('App', function() {
+describe('GET ALL USERS request of App:', function() {
   after(() => {
     stop();
   });
 
-  it('it should GET users', (done) => {
+  it('Response status is 200', (done) => {
     chai.request(app)
       .get('/api/users')
       .end((err, res) => {
         res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('Response is array', (done) => {
+    chai.request(app)
+      .get('/api/users')
+      .end((err, res) => {
         res.body.should.be.a('array');
-        res.body.length.should.be.eql(users.length);
+        done();
+      });
+  });
+
+  it('Response array is equal to test array', (done) => {
+    chai.request(app)
+      .get('/api/users')
+      .end((err, res) => {
         res.body.should.be.eql(users);
-
-        done();
-      });
-  });
-
-  it('it should GET catalog', (done) => {
-    chai.request(app)
-      .get('/api/categories')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body.length.should.be.eql(catalog.length);
-        res.body.should.be.eql(catalog);
-
-        done();
-      });
-  });
-
-  it('it should GET catalog', (done) => {
-    chai.request(app)
-      .get('/api/categories')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body.length.should.be.eql(catalog.length);
-        res.body.should.be.eql(catalog);
-
         done();
       });
   });
@@ -69,6 +58,158 @@ describe('App', function() {
         res.body.should.be.a('object');
         res.body.should.be.eql(products[0]);
 
+        done();
+      });
+  });
+});
+
+describe('GET CATALOG request of App:', function() {
+  after(() => {
+    stop();
+  });
+
+  it('Response status is 200', (done) => {
+    chai.request(app)
+      .get('/api/categories')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('Response is array', (done) => {
+    chai.request(app)
+      .get('/api/categories')
+      .end((err, res) => {
+        res.body.should.be.a('array');
+        done();
+      });
+  });
+
+  it('Response array is equal to test array', (done) => {
+    chai.request(app)
+      .get('/api/categories')
+      .end((err, res) => {
+        res.body.should.be.eql(catalog);
+        done();
+      });
+  });
+});  
+
+describe('GET FIRST PRODUCT request of App:', function() {
+  after(() => {
+    stop();
+  });
+
+  it('Response status is 200', (done) => {
+    chai.request(app)
+      .get('/api/products/1')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('Response is array', (done) => {
+    chai.request(app)
+      .get('/api/categories')
+      .end((err, res) => {
+        res.body.should.be.a('array');
+        done();
+      });
+  });
+
+  it('Response array is equal to test array', (done) => {
+    chai.request(app)
+      .get('/api/products/1')
+      .end((err, res) => {
+        res.body.should.be.eql(products[0]);
+        done();
+      });
+  });
+}); 
+
+describe('POST LOGIN request of App for existent user:', function() {
+  after(() => {
+    stop();
+  });
+
+  it('Response status is 200', (done) => {
+    chai.request(app)
+      .post('/api/login')
+      .type('form')
+      .send({
+        'email': 'user@user.com',
+        'password': 'user'
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('Response logged user is equal to a test user', (done) => {
+    chai.request(app)
+      .post('/api/login')
+      .type('form')
+      .send({
+        "email": "user@user.com",
+        "password": "user"
+      })
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        res.body.should.be.eql(users[0]);
+        done();
+      });
+  });
+});
+
+describe('POST LOGIN request of App for nonexistent user:', function() {
+  after(() => {
+    stop();
+  });
+
+  it('Response status is 401', (done) => {
+    chai.request(app)
+      .post('/api/login')
+      .type('form')
+      .send({
+        'email': 'user@u',
+        'password': 'user123'
+      })
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
+  });
+
+  it('Response is object', (done) => {
+    chai.request(app)
+      .post('/api/login')
+      .type('form')
+      .send({
+        'email': 'nosuchuser@fantom.com',
+        'password': 'nosuchpasword',
+      })
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        done();
+      });
+  });
+
+  it('Response object is equal to test object', (done) => {
+    chai.request(app)
+      .post('/api/login')
+      .type('form')
+      .send({
+        'email': 'nosuchuser@fantom.com',
+        'password': 'nosuchpasword',
+      })
+      .end((err, res) => {
+        res.body.should.be.eql({
+          "success": false,
+          "message": "Invalid email or password."
+      });
         done();
       });
   });
