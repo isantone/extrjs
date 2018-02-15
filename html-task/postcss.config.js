04288aa232de0c108300b9e3092b3bd0a115c230
Anton_Isakov <anton_isakov@epam.com>
2018-02-15 12:57:29 +0400

    Mq-packer added to bundle

--- html-task/postcss.config.js
+++ html-task/postcss.config.js
@@ -1,11 +1,13 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const mqpacker = require("css-mqpacker");
 
 module.exports = {
   parser: 'postcss-scss',
   map: true,
   plugins: [
     autoprefixer,
+    mqpacker,
     cssnano
   ]
 };
