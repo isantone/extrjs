58974fc927f51ffeb031a74564c702b8bda3b8a6
Anton Isakov <anton_isakov@epam.com>
2018-03-08 16:21:47 +0400

    Other projects removed

--- bom-bt-task/Task_3/js/window.js
+++
@@ -1,9 +0,0 @@
-function openNewWindow(width, height) {
-  'use strict';
-  let windowParams = "width=" + width + ",height=" + height;
-  let newWindow = window.open("about:blank", "New Window", windowParams);
-}
-
-function enableButton(id) {
-  id.removeAttribute("disabled");
-}
